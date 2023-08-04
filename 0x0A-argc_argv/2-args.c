4dc707c537bba7e627a7877226d600634e606d7e
Vianne Iheme <vianneiheme@yahoo.com>
2023-08-04 08:47:36 -0700

    mynameis

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,16 +2,19 @@
 #include "main.h"
 
 /**
- * main - prints the name of the program, followed by a new line.
+ * main - prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-(void) argc;
-printf("%s\n", argv[0]);
+int i;
+for (i = 0; i < argc; i++)
+{
+	printf("%s\n", argv[i]);
+}
 return (0);
 }
