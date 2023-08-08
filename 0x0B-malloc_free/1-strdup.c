a060d738b8ec0ff728281ba654a54efefe819da2
Vianne Iheme <vianneiheme@yahoo.com>
2023-08-08 12:28:18 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates to new memory space location
+ * @str: char
+ * Return: 0
+ *
+ */
+
+char *_strdup(char *str)
+{
+char *aaa;
+int i, j = 0;
+if (str == NULL)
+	return (NULL);
+i = 0;
+while (str[i] != '\0')
+	i++;
+aaa = malloc(sizeof(char) * (i + 1));
+if (aaa == NULL)
+return (NULL);
+for (j = 0; str[j]; j++)
+	aaa[j] = str[j];
+return (aaa);
+}
+
